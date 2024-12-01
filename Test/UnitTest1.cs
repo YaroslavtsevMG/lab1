bcee8bd450fea7479a706107b58b66fb2bec4edb
matveyyaros <matveyyaros0112@gmail.com>
2024-12-01 15:30:58 +0500

    .

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,7 +8,14 @@ namespace Test
         public void TestMethod1()
         {
             var classofliblary = new ArithmaticClass();
-            Assert.AreEqual(classofliblary.z_func1(5,6,9), 62.05104198);
+            Assert.AreEqual(classofliblary.z_func1(5,6,9), 62.05104446411133);
+            
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var classofliblary = new ArithmaticClass();
+            Assert.AreEqual(classofliblary.z_func2(1, 1, 1), 4);
         }
     }
 }
